315c612dfb4531bc09234a9937eb82e34aa7c61c
Brody Gaston <gaston.brody@gmail.com>
2025-04-17 11:53:16 -0700

    Assignment 2 Halfway Done

---
+++ Assignment2/JS/darkmode.js
@@ -0,0 +1,31 @@
+$('document').ready(function () {
+
+    let darkmode = localStorage.getItem('darkmode')
+    const themeSwitch = document.getElementById('theme-switch')
+
+    const enableDarkmode = () => {
+
+        document.body.classList.add('darkmode')
+        localStorage.setItem('darkmode', 'active')
+        document.getElementById('title').innerHTML = "Click Here to Change to LightMode!"
+
+    }
+
+    const disableDarkmode = () => {
+
+        document.body.classList.remove('darkmode')
+        localStorage.setItem('darkmode', null)
+        document.getElementById('title').innerHTML = "Click Here to Change to DarkMode!"
+
+    }
+
+    if (darkmode === "active") enableDarkmode()
+
+    themeSwitch.addEventListener("click", () => {
+
+        darkmode = localStorage.getItem('darkmode')
+        darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+
+    })
+    
+})
