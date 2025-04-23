84b2e59a8b0b88602839eedf9557c1a1a8082d08
Brody Gaston <gaston.brody@gmail.com>
2025-04-23 01:30:42 -0700

    Ugh

---
+++ Assignment3/responsive.js
@@ -0,0 +1,39 @@
+function setup() {
+    createCanvas(500, 500);
+    noCursor();
+
+    strokeWeight(5);
+
+    osc = new p5.TriOsc();
+  }
+  
+  function draw() {
+
+    noStroke();
+
+    background('#788585');
+  
+    fill('#af280b');
+    triangle(500, 500, 500, 0, 150, 250);
+  
+    fill('#ece4d7');
+    triangle(375, 90, 150, 0, 150, 250);
+  
+    fill('#7e7a79');
+    triangle(500, 0, 150, 0, 375, 90);
+  
+    fill('#c6cad5');
+    triangle(500, 500, 150, 500, 150, 250);
+  
+    fill('#d7a601');
+    triangle(75, 375, 0, 250, 0, 500);
+    
+    fill('#c6cad5');
+    triangle(150, 0, 0, 0, 0, 250);
+    
+    fill('black');
+    triangle(150, 500, 150, 0, 0, 250);
+    
+    fill('#ece4d7');
+    triangle(150, 500, 75, 375, 0, 500);
+  }
--- Assignment3/sketch.js
+++ Assignment3/sketch.js
@@ -3,5 +3,31 @@ function setup() {
 }
 
 function draw() {
-  background('aqua');
+
+  noStroke();
+  background('black');
+
+  fill('#af280b');
+  triangle(500, 500, 500, 0, 150, 250);
+
+  fill('#ece4d7');
+  triangle(375, 90, 150, 0, 150, 250);
+
+  fill('#7e7a79');
+  triangle(500, 0, 150, 0, 375, 90);
+
+  fill('#c6cad5');
+  triangle(500, 500, 150, 500, 150, 250);
+
+  fill('#d7a601');
+  triangle(150, 500, 0, 250, 0, 500);
+  
+  fill('#c6cad5');
+  triangle(150, 0, 0, 0, 0, 250);
+  
+  fill('black');
+  triangle(150, 500, 150, 0, 0, 250);
+  
+  fill('#ece4d7');
+  triangle(150, 500, 75, 375, 0, 500);
 }
